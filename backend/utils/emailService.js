608abeb180720e8a92997f6c0201c5e7f7740bd0
Arpit Omre <arpitomre@gmail.com>
2026-08-03 18:27:43 +0530

    configure brevo SMTP

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -19,7 +21,7 @@ const sendEmail = async (to, subject, html) => {
 
         console.log("Email sent successfully");
     } catch (error) {
-        console.log("Email Error:", error);
+        console.error("Email Error:", error);
     }
 };
 
